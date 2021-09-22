72930c95a759056635418865b2a3b670efa893a2
cataclysm78 <42520898+moshkof@users.noreply.github.com>
2021-09-22 21:41:01 +0300

    three commit

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor 
 # from token import TOKEN
 
-bot = Bot(token = "1611700510:AAH5poM-n5bLAHaxcTsBzSOCk38Qa64gsSE")
+bot = Bot(token="1611700510:AAH5poM-n5bLAHaxcTsBzSOCk38Qa64gsSE")
 
 dp = Dispatcher(bot)
 
@@ -13,4 +13,4 @@ async def get_message(message: types.Message):
     sent_message = await bot.send_message(chat_id, text=text)
     print(sent_message.to_python())
 
-executor.start_polling()
+executor.start_polling(dp)
