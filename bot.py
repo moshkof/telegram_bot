840968572bdf205bca02cc74fb4436404d177c17
cataclysm78 <cataclysm78@yandex.ru>
2021-09-24 17:40:44 +0300

    Добавлен config с токеном

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor 
+from aiogram.utils import executor
 # from token import TOKEN
+import config
 
-bot = Bot(token = "1611700510:AAH5poM-n5bLAHaxcTsBzSOCk38Qa64gsSE")
+bot = Bot(token=config.TOKEN)
 
 dp = Dispatcher(bot)
 
+
 @dp.message_handler()
 async def get_message(message: types.Message):
-    chat_id = message.chat_id
-    text = "1611700510:AAH5poM-n5bLAHaxcTsBzSOCk38Qa64gsSE"
-    sent_message = await bot.send_message(chat_id, text=text)
+    chat_id = message.chat.id
+    text = "Привет, "
+    sent_message = await bot.send_message(chat_id, text=(text + message.chat.last_name + " " + message.chat.first_name + "!"))
     print(sent_message.to_python())
 
-executor.start_polling()
+executor.start_polling(dp)
