dd9d1d23c90edc523196f015a7e9b4db9d9b7345
cataclysm78 <cataclysm78@yandex.ru>
2021-09-24 17:48:27 +0300

    dev 1

--- bot.py
+++ bot.py
@@ -3,7 +3,11 @@ from aiogram.utils import executor
 # from token import TOKEN
 import config
 
+<<<<<<< HEAD
 bot = Bot(token=config.TOKEN)
+=======
+bot = Bot(token="1611700510:AAH5poM-n5bLAHaxcTsBzSOCk38Qa64gsSE")
+>>>>>>> 72930c95a759056635418865b2a3b670efa893a2
 
 dp = Dispatcher(bot)
 
@@ -15,4 +19,8 @@ async def get_message(message: types.Message):
     sent_message = await bot.send_message(chat_id, text=(text + message.chat.last_name + " " + message.chat.first_name + "!"))
     print(sent_message.to_python())
 
+<<<<<<< HEAD
 executor.start_polling(dp)
+=======
+executor.start_polling(dp)
+>>>>>>> 72930c95a759056635418865b2a3b670efa893a2
