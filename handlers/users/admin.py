6f687b4ec61510e00be61f974dc5a3260ad724e8
cataclysm78 <cataclysm78@yandex.ru>
2021-11-09 12:34:57 +0300

    Поправлен hendlers/users/hi

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -2,10 +2,10 @@ from aiogram import types
 from filters import IsPrivate
 from loader import dp
 
-from data.config import admins
+from data.config import ADMINS
 
 
-@dp.message_handler(IsPrivate(), text="secret", user_id=admins)
+@dp.message_handler(IsPrivate(), text="secret", user_id=ADMINS)
 async def admin_chat_secret(message: types.Message):
     await message.answer("Это секретное сообщение вызванное одним из администраторов "
                          "в личной переписке.")
